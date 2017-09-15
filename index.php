d44f2ae4b06ac4238075769d2c3045a81ea38485
Valvin <git@valvin.fr>
2017-09-15 22:34:56 +0200

    add appcache log info

--- index.php
+++ index.php
@@ -3,6 +3,58 @@
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="gege.css"/>
+<script>
+(function() {
+	var webappCache = window.applicationCache;
+	
+	function loaded()
+	{
+		//var h1El = document.querySelector("h1");
+		var connectionStatus = ((navigator.onLine) ? 'online' : 'offline');
+		//h1El.textContent = h1El.textContent + " - currently: " + connectionStatus;
+
+		switch(webappCache.status)
+		{
+			case 0:
+				console.log("Cache status: Uncached");
+				break;
+			case 1:
+				console.log("Cache status: Idle");
+				break;
+			case 2:
+				console.log("Cache status: Checking");
+				break;
+			case 3:
+				console.log("Cache status: Downloading");
+				break;
+			case 4:
+				console.log("Cache status: Updateready");
+				break;
+			case 5:
+				console.log("Cache status: Obsolete");
+				break;
+		}
+
+	}
+
+	function updateCache()
+	{
+		webappCache.swapCache();
+		console.log("Cache has been updated due to a change found in the manifest");
+	}
+
+	function errorCache()
+	{
+		console.log("You're either offline or something has gone horribly wrong.");
+	}
+
+	window.addEventListener("load", loaded, false);
+	webappCache.addEventListener("updateready", updateCache, false);
+	webappCache.addEventListener("error", errorCache, false);
+
+})();
+</script>
+
 <title>GéGé, Geektionnerd Generator</title>
  </head>
 <body>
